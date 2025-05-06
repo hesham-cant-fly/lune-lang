9a0d9bd33222db11c5235737f7a1a4eb16450763
hesham-cant-fly <www.hichamhot@gmail.com>
2025-05-06 01:10:15 +0100

    TODO: Use zig-clap

--- src/main.zig
+++ src/main.zig
@@ -22,6 +22,10 @@ pub const termcolor = @import("./termcolor.zig");
 const str = []const u8;
 
 pub fn main() !void {
+    if (std.os.argv.len == 1) {
+        std.debug.print("needs some arguments\n", .{});
+        return;
+    }
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = gpa.allocator();
     defer _ = gpa.deinit();
@@ -29,14 +33,18 @@ pub fn main() !void {
     var arena = std.heap.ArenaAllocator.init(allocator);
     defer arena.deinit();
 
-    const content = try read_file_to_slice(allocator, "main.lune");
+    const file_name_zero = std.os.argv[1];
+    var len: usize = 0;
+    while (file_name_zero[len] != 0) : (len += 1) {}
+    const file_name = file_name_zero[0..len];
+    const content = try read_file_to_slice(allocator, file_name);
     defer allocator.free(content);
 
-    var lxr = try Lexer.init(allocator, content, "main.lune");
+    var lxr = try Lexer.init(allocator, content, file_name);
     errdefer lxr.deinit();
 
     const tokens = try lxr.scan();
-    var parser = Parser.init(arena.allocator(), tokens.items, content, "main.lune");
+    var parser = Parser.init(arena.allocator(), tokens.items, content, file_name);
     const ast = try parser.parse();
     lxr.deinit();
 
@@ -48,7 +56,7 @@ pub fn main() !void {
     //     },
     // });
 
-    var analyzer = try Analyzer.init(arena.allocator(), ast, content, "main.lune");
+    var analyzer = try Analyzer.init(arena.allocator(), ast, content, file_name);
     defer analyzer.deinit();
 
     const tsast = try analyzer.analyze();
@@ -56,15 +64,15 @@ pub fn main() !void {
     const res = try tr.compile(.Lua);
     defer res.deinit();
 
-    const out_file = try std.fs.cwd().createFile("./out.lua", .{
-        .truncate = true,
-    });
-    defer out_file.close();
+    // const out_file = try std.fs.cwd().createFile("./out.lua", .{
+    //     .truncate = true,
+    // });
+    // defer out_file.close();
 
-    try out_file.writeAll(res.items);
+    // try out_file.writeAll(res.items);
 
     // std.time.sleep(std.time.ns_per_s * 30);
-    // std.debug.print("{s}\n", .{res.items});
+    std.debug.print("{s}\n", .{res.items});
 }
 
 pub fn read_file_to_slice(allocator: mem.Allocator, path: str) !str {
